75636b01f16b0ec2f1ec04fc3c0026f3b41cf78e
tiagosiebler <tiagosiebler@github.com>
2019-06-05 11:12:44 +0100

    add example for rendering report results using an existing style & the ReportBean

--- stand-alone/com/microstrategy/samples/util/FileHelper.java
+++ stand-alone/com/microstrategy/samples/util/FileHelper.java
@@ -2,6 +2,8 @@ package com.microstrategy.samples.util;
 
 import java.io.*;
 
+import com.microstrategy.web.beans.MarkupOutput;
+
 public class FileHelper {
   /*
    * Helper function to save an in-memory object to file
@@ -25,6 +27,23 @@ public class FileHelper {
         e.printStackTrace();
       }
     }
+  }
+  
+  // Fetch contents of MarkupOutput object and save to file
+  public static void saveMarkupOutputToFile(MarkupOutput markupOutput, String targetFilePath) {
+    boolean isBinary = markupOutput.getBinaryMode();
+    if (isBinary) {
+      // get binary data
+      byte[] bytes = markupOutput.getBinaryContent();
+      saveByteArrayToFile(bytes, targetFilePath);
+      return;
+    }
+    
+    // get string contents of MarkupOutput
+    String results = markupOutput.getCopyAsString();
+    byte[] stringBytes = results.getBytes();
+    
+    saveByteArrayToFile(stringBytes, targetFilePath);
   }
 
 }

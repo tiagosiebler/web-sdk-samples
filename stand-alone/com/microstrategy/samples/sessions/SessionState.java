e8bfd58081c9b12abe92e5fd45310462830b5e35
tiagosiebler <tiagosiebler@github.com>
2019-07-08 16:50:05 +0100

    use explanatory vars instead

--- stand-alone/com/microstrategy/samples/sessions/SessionState.java
+++ stand-alone/com/microstrategy/samples/sessions/SessionState.java
@@ -4,13 +4,17 @@ import com.microstrategy.utils.serialization.EnumWebPersistableState;
 import com.microstrategy.web.objects.WebIServerSession;
 
 public class SessionState {
+  private static String DEFAULT_SERVER = "APS-TSIEBLER-VM";
+  private static String DEFAULT_PROJECT = "MicroStrategy Tutorial";
+  private static String DEFAULT_USERNAME = "Administrator";
+  private static String DEFAULT_PASSWORD = "";
   
   /*
    * Simple demonstration on saving a session as a "session state" string, which can be used with other requests (e.g the task API or URL API).
    */
   public static void main(String[] args) {
     // create a session in any way
-    WebIServerSession exampleSession = SessionManager.getSession();
+    WebIServerSession exampleSession = SessionManager.getSessionWithDetails(DEFAULT_SERVER, DEFAULT_PROJECT, DEFAULT_USERNAME, DEFAULT_PASSWORD);
     
     // save the session state as a string
     String sessionStateString = getSessionState(exampleSession);

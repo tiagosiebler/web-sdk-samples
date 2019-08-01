a4ae75a68626258d308852d2b9d887bdbad25b81
Enrique Pastrana <enrique.microstrategy@gmail.com>
2019-08-01 10:56:16 +0200

    Corrected changes for PR

--- stand-alone/com/microstrategy/samples/beans/rwbean/RetrieveDocumentImages.java
+++ stand-alone/com/microstrategy/samples/beans/rwbean/RetrieveDocumentImages.java
@@ -21,87 +21,80 @@ public class RetrieveDocumentImages {
 
 	public static void main(String[] args) throws WebBeanException, WebObjectsException {
 		// Connectivity for the intelligence server
-	    String intelligenceServerName = "10.23.5.242";
-	    String projectName = "MicroStrategy Tutorial";
-	    String microstrategyUsername = "Administrator";
-	    String microstrategyPassword = "";
-	    
-	    // The GUID of the document to use
-	    String documentID = "911D069848D92F6A07AF65A7658E97A0";
-	    
-	    // Create our I-Server Session
-	    WebIServerSession session = SessionManager.getSessionWithDetails(intelligenceServerName, projectName, microstrategyUsername, microstrategyPassword);
+		String intelligenceServerName = "10.23.5.242";
+		String projectName = "MicroStrategy Tutorial";
+		String microstrategyUsername = "Administrator";
+		String microstrategyPassword = "";
 
-	    //Array with the images objects.
-	    ArrayList<WebBlob> imagesArrayList = getEmbeddedImagesFromDocument(documentID, session);
-	    
-	    //Demo usage: Saving the images to file system.
-	    //Path to save the images.
-	    String pathString = "/USers/mpastrana";
-	    saveImagesTofile(imagesArrayList, pathString);
+		// The GUID of the document to use
+		String documentID = "911D069848D92F6A07AF65A7658E97A0";
+
+		// Create our I-Server Session
+		WebIServerSession session = SessionManager.getSessionWithDetails(intelligenceServerName, projectName, microstrategyUsername, microstrategyPassword);
+
+		// Array with the images objects.
+		ArrayList<WebBlob> imagesArrayList = getEmbeddedImagesFromDocument(documentID, session);
+
+		// Path to save the images.
+		String pathString = "/Users/mpastrana";
+		saveImagesTofile(imagesArrayList, pathString);
+
+	}
+
+	// Extract images embedded in a document using its ID (documentID) and an existing session (session) to an ArrayList of images.
+	public static ArrayList<WebBlob> getEmbeddedImagesFromDocument(String documentID, WebIServerSession session) throws WebObjectsException, IllegalArgumentException, WebBeanException {
+
+		RWBean rwb = (RWBean)BeanFactory.getInstance().newBean("RWBean");
+
+		// ID of the object we wish to execute
+		rwb.setObjectID(documentID);
+
+		// Session used for execution
+		rwb.setSessionInfo(session);
+
+		// Create Document instance.
+		WebObjectsFactory objectsFactory = WebObjectsFactory.getInstance();
+		RWInstance rwInstance = rwb.getRWInstance();        
+
+		System.out.println("Document Name: " + rwInstance.getDefinition().getName());
+
+		// Retrieve document object definition to retrieve images.
+		RWDefinition rwDefinition = rwInstance.getDefinition();
+
+		// Filter object by image type (EnumRWUnitTypes.RWUNIT_IMAGE)
+		RWUnitDef[] rwUnitDefs = rwDefinition.findUnitsByType(EnumRWUnitTypes.RWUNIT_IMAGE);
+
+		// Prints number of images.
+		System.out.println("Number of images in document: " + rwUnitDefs.length);
+
+		// Creating the ArrayList of WebBlog images objects.
+		WebObjectSource webObjectSource = objectsFactory.getObjectSource(); 
+		
+		// Images in MicroStrategy metadata are stored in WebBlob objects.
+		// https://lw.microstrategy.com/msdz/MSDL/GARelease_Current/docs/ReferenceFiles/reference/com/microstrategy/web/objects/WebBlob.html
+		// Looping through all the objects to compose the images ArrayList.
+		ArrayList<WebBlob> imagesBlobs = new ArrayList<WebBlob>();
+		for (RWUnitDef rwUnitDef : rwUnitDefs) {
+			RWImageDef imageBlobImpl = (RWImageDef)rwUnitDef;
+			String imageObjectID = imageBlobImpl.getPath();
+			WebBlob imageBlob = (WebBlob)webObjectSource.getObject(imageObjectID, EnumDSSXMLObjectTypes.DssXmlTypeBlob);
+			imagesBlobs.add(imageBlob);
+		}
+
+		//Return ArrayList of images.
+		return imagesBlobs;
 
 	}
 	
-	
-	 public static ArrayList<WebBlob> getEmbeddedImagesFromDocument(String documentID, WebIServerSession session) throws WebObjectsException, IllegalArgumentException, WebBeanException {
-		    
-		    RWBean rwb = (RWBean)BeanFactory.getInstance().newBean("RWBean");
-		    
-		    // ID of the object we wish to execute
-		    rwb.setObjectID(documentID);
-		    
-		    // session used for execution
-		    rwb.setSessionInfo(session);
-		    
-		    //Create Document instance.
-		    WebObjectsFactory objectsFactory = WebObjectsFactory.getInstance();
-			RWInstance rwInstance = rwb.getRWInstance();        
-			
-			//Show document's name.
-			System.out.println("Document Name: " + rwInstance.getDefinition().getName());
-			
-			//Retrieve document object definition to retrieve images.
-			RWDefinition rwDefinition = rwInstance.getDefinition();
-			
-			//Filter object by image type (EnumRWUnitTypes.RWUNIT_IMAGE)
-			RWUnitDef[] rwUnitDefs = rwDefinition.findUnitsByType(EnumRWUnitTypes.RWUNIT_IMAGE);
+	// Save images from imageList ArrayList to file system. Full path for every image is composed using pathString + index of element in the ArrayList.  
+	public static void saveImagesTofile(ArrayList<WebBlob> imageList, String pathString) throws WebObjectsException {
+		String imgPrefixString = "Image";
 
-			//Prints number of images.
-			System.out.println("Number o fimages in document: " + rwUnitDefs.length);
-			
-			//Creating the ArrayList of WebBlog images objects.
-			RWImageDef imageBlobImpl = null;
-			String pathString = null;
-			WebObjectSource webObjectSource = null;
-			ArrayList<WebBlob> imagesBlobs = new ArrayList<WebBlob>();
-			for (RWUnitDef rwUnitDef : rwUnitDefs) {
-				imageBlobImpl = (RWImageDef)rwUnitDef;
-				pathString = imageBlobImpl.getPath();
-				webObjectSource = objectsFactory.getObjectSource();
-				WebBlob imageBlob = (WebBlob)webObjectSource.getObject(pathString, EnumDSSXMLObjectTypes.DssXmlTypeBlob);
-				imagesBlobs.add(imageBlob);
-			}
-			
-			//Return ArrayList of images.
-			return imagesBlobs;
-			
-	  }
-	 
-	 public static void saveImagesTofile(ArrayList<WebBlob> imageList, String pathString) throws WebObjectsException {
-		 //Receives an ArrayList of images and print them setting the name: Image + index in the array
-		 String imgPrefixString = "Image";
-		 
-		 
-		 for (WebBlob imageBlob : imageList) {
+		for (WebBlob imageBlob : imageList) {
 			byte[] image = imageBlob.getBlob();
 			String fileName = imgPrefixString + imageList.indexOf(imageBlob);
 			FileHelper.saveByteArrayToFile(image, pathString + fileName);
 		}
-		 
-	
-		
-		
-		
-	 }
+	}
 
 }

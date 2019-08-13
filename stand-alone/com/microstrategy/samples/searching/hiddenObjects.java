d5e84cf771a4424bcb9990d313a83fb1f571822f
Owcaman <esokolowski@microstrategy.com>
2019-08-13 17:14:11 +0200

    Final trim with Tiago (quite a lot of things have been changed).

--- stand-alone/com/microstrategy/samples/searching/hiddenObjects.java
+++ stand-alone/com/microstrategy/samples/searching/hiddenObjects.java
@@ -1,24 +1,16 @@
 /* FOREWORD:
 
-The code enables searching through a search directory for folders that can either be visible-only or hidden.
+This sample searches through a parent folder for folders that can either be visible-only or hidden.
 The change is only a matter of commenting out the object flag on search: EnumDSSXMLObjectFlags.DssXmlObjectFindHidden
-If ObjectFindHidden is on - it find all files (including the hidden ones) If the flag is not there - only visible
-folders will be returned. Later it also lists the folders with all the details. 
-
-You can also use it for searching for other types of files (apart from folders) by changing the flag on searchTypes: 
-search.types().add(EnumDSSXMLObjectTypes.DssXmlTypeFolder). 
+If provided, hidden and visible folders will be found. If not, only visible folders will be found.
 
 The information that you have to fill in is: 
 1. The package
-2. The details of te I-Server session
+2. The details of the I-Server session
 3. The GUID of the folder that you want to search through - the sample one used is the Public Objects folder.
 
-Apart from those details the code is ready to run.
-
 Made with love by Edgar, based on [278906/DE124645/KB16830]
-Did not use object.populate() but search.getResults() instead. Chose to search folders as needed it for practical purposes.
-
-Find the code below, and ENJOY. */
+*/
 
 package com.microstrategy.samples.searching;
 
@@ -30,77 +22,76 @@ import com.microstrategy.web.objects.WebIServerSession;
 import com.microstrategy.web.objects.WebObjectInfo;
 import com.microstrategy.web.objects.WebObjectSource;
 import com.microstrategy.web.objects.WebObjectsException;
-import com.microstrategy.web.objects.WebObjectsFactory;
 import com.microstrategy.web.objects.WebSearch;
-import com.microstrategy.webapi.EnumDSSXMLApplicationType;
 import com.microstrategy.webapi.EnumDSSXMLObjectFlags;
 import com.microstrategy.webapi.EnumDSSXMLObjectTypes;
 import com.microstrategy.webapi.EnumDSSXMLSearchFlags;
 
 public class hiddenObjects {
 
-	public static void main(String[] args) throws WebObjectsException { 
-
-		// Starting the session
-		WebIServerSession serverSession = startTheIServerSession();
-		
-		// Performing the search
-		performTheSearch(serverSession);
-
-	}
-	
-	public static WebIServerSession startTheIServerSession() {
-	    
-		System.out.println("Initiating the session.");
-
-		// Connectivity details for the intelligence server
-		String intelligenceServerName = "SERVER";
-		String projectName = "MicroStrategy Tutorial";
-		String microstrategyUsername = "Administrator";
-		String microstrategyPassword = "";
+  public static void main(String[] args) throws WebObjectsException { 
+    // Connectivity details for the intelligence server
+    String intelligenceServerName = "aps-tsiebler-vm";
+    String projectName = "MicroStrategy Tutorial";
+    String microstrategyUsername = "Administrator";
+    String microstrategyPassword = "";
     
-		// Creating our I-Server Session
-		WebIServerSession serverSession = SessionManager.getSessionWithDetails(intelligenceServerName, projectName, microstrategyUsername, microstrategyPassword);
-		
-		return serverSession;
-	  }
-	
-	public static void performTheSearch(WebIServerSession serverSession) throws WebObjectsException {
-	    
-		System.out.println("Performing the search.");
+    // Parent folder we want to search within. The sample ID points to the Public Objects folder in the MicroStrategy Tutorial project.
+    String parentFolderObjectID = "98FE182C2A10427EACE0CD30B6768258"; 
 
-		WebObjectSource source = serverSession.getFactory().getObjectSource();
-		WebSearch search = source.getNewSearchObject();
+    // Create our Intelligence Server Session
+    System.out.println("Connecting to intelligence server...");
+    WebIServerSession serverSession = SessionManager.getSessionWithDetails(intelligenceServerName, projectName, microstrategyUsername, microstrategyPassword);
 
-		String folderGUID = "98FE182C2A10427EACE0CD30B6768258"; 
+    // Execute a search for folders, including hidden folders, and return the results
+    WebFolder searchResults = searchForFoldersWithinFolder(serverSession, parentFolderObjectID);
 
-		search.setSearchRoot(folderGUID);
-		search.setFlags(EnumDSSXMLSearchFlags.DssXmlSearchNameWildCard | EnumDSSXMLSearchFlags.DssXmlSearchRootRecursive | EnumDSSXMLObjectFlags.DssXmlObjectFindHidden);
-		search.setAsync(false);
+    // Print all folders found in search
+    printSearchResults(searchResults);
+  }
 
-		search.types().add(EnumDSSXMLObjectTypes.DssXmlTypeFolder);
-		search.submit();	
+  public static WebFolder searchForFoldersWithinFolder(WebIServerSession serverSession, String parentFolderObjectID) throws WebObjectsException {
+    
+	System.out.println("Performing the search.");
+    
+    WebObjectSource source = serverSession.getFactory().getObjectSource();
+    
+    // Prepare a new search using the WebSearch object. The WebObjectSource connects this to the session provided above
+    WebSearch search = source.getNewSearchObject();
+    
+    // Look for any results, within this folder in the metadata (or any child of that folder)
+    search.setSearchRoot(parentFolderObjectID);
+    
+    // Flags that affect how this search will be done
+    search.setFlags(EnumDSSXMLSearchFlags.DssXmlSearchNameWildCard | EnumDSSXMLSearchFlags.DssXmlSearchRootRecursive | EnumDSSXMLObjectFlags.DssXmlObjectFindHidden);
+    
+    // Wait until results are ready
+    search.setAsync(false);
 
-		System.out.println("Search submitted.");
+    // Limit search results to only objects of type: folder. Any other object types are ignored.
+    search.types().add(EnumDSSXMLObjectTypes.DssXmlTypeFolder);
+    
+    // Execute the search against the metadata
+    search.submit();	
 
-		WebFolder folderWithResults = search.getResults();
-		System.out.println("Filtering the results.");
+    System.out.println("Search submitted, returning results");
+    return search.getResults();
+  }
 
-		int size = folderWithResults.size();
+  public static void printSearchResults(WebFolder searchResults) {
+    
+	System.out.println("This search found " + searchResults.size() + " results");
 
-		System.out.println("All of the folders from within the search directory: ");
+    // Getting a list of results into Enumeration
+    Enumeration<WebObjectInfo> results = searchResults.elements();
 
-		// Getting a list of results into Enumeration
-		Enumeration<WebObjectInfo> results = folderWithResults.elements();
-
-		// Iterating through each result, while results remain
-		while (results.hasMoreElements()) {
-			WebObjectInfo result = results.nextElement();
-			System.out.println(result.getName());
-		}
-		
-		System.out.println("Size of the folder is: " + size);
-		
-	  }
-}	
-
+    // Iterating through each result, while results remain
+    System.out.println("Looping through results by name: ");
+    while (results.hasMoreElements()) {
+      WebObjectInfo result = results.nextElement();
+      System.out.println(result.getName());
+    }
+    
+    System.out.println("Finished looping through search results.");
+  }
+}
